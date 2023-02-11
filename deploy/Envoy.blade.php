07c6700f89af69a04b813b60592da28423e31538
Wesleygsantos <wesleyg_santos@yahoo.com.br>
2023-02-11 12:01:41 -0300

    teste de commit servidor

---
+++ deploy/Envoy.blade.php
@@ -0,0 +1,40 @@
+@servers(['web' => ['wesleygsantos@wesleygsantos.eu1.alfasoft.pt']])
+
+@setup
+    $path = "/cloudcmd/"
+@endsetup
+ 
+@story('deploy')
+    git
+    composer
+    config
+    route
+    restart
+@endstory
+ 
+@task('git')
+    cd {{ path }}
+
+    git reset --hard origin/main
+    git pull origin main
+@endtask
+ 
+@task('composer')
+    cd {{ path }}
+
+    composer install --optimize-autoloader --no-dev
+@endtask
+
+@task('config')
+    cd {{ path }}
+
+    php artisan config:cache
+@endtask
+
+@task('route')
+    cd {{ path }}
+
+    php artisan route:cache
+@endtask
+
+

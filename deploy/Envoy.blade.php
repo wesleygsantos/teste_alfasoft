db2320541b49a99cbea999b7e939d8e140247a80
Wesleygsantos <wesleyg_santos@yahoo.com.br>
2023-02-11 12:34:55 -0300

    correcao

--- deploy/Envoy.blade.php
+++ deploy/Envoy.blade.php
@@ -13,26 +13,26 @@
 @endstory
  
 @task('git')
-    cd {{ path }}
+    cd {{ $path }}
 
     git reset --hard origin/main
     git pull origin main
 @endtask
  
 @task('composer')
-    cd {{ path }}
+    cd {{ $path }}
 
     composer install --optimize-autoloader --no-dev
 @endtask
 
 @task('config')
-    cd {{ path }}
+    cd {{ $path }}
 
     php artisan config:cache
 @endtask
 
 @task('route')
-    cd {{ path }}
+    cd {{ $path }}
 
     php artisan route:cache
 @endtask
